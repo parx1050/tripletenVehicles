02adfc545eee1669fb7a97608fe3fea075d7bdd7
parx1050 <parker.shyu@gmail.com>
2024-03-05 03:49:50 -0500

    Completed the README.md file & app.py file, and edited the notebooks/EDA.ipynb file.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # tripletenVehicles
+
+Hello! Welcome to my Sprint 4 Project: A Sample Application
+
+This application can be run with Streamlit on a local server or with Render: {link}
+
+The following details the heirarchy of the project:
+├── README.md
+├── app.py
+├── vehicles_us.csv
+└── notebooks
+    └── EDA.ipynb
+└── .streamlit
+    └── config.toml
+└── .conda
+    └── ...
+├── .gitignore
+
+The README.md is this current file, which details everything in the project.
+The app.py file contains the Python code used to run the application. You can run the application from your system console with the "$ streamlit app.py run" command.
+The EDA.ipynb file is a Jupyter Notebooks file that performs basic EDA analysis on the dataset, plotting histograms and displaying info for the data.
+The vehicles_us.csv file contains the dataset for this project, detailing about 51,000 different data points for vehicles, and columns for:
+    - price
+    - model_year
+    - model
+    - condition
+    - cylinders
+    - fuel
+    - odometer
+    - transmission
+    - type
+    - paint_color
+    - is_4wd
+    - date_posted
+    - days_listed
+All other files are background functions for the execution of the project.
+
+The project itself provides three options for users to interact with the dataset:
+    - The user can look through the whole dataset formatted as a Pandas DataFrame, and apply any combination of the four following filters to the dataset:
+        - Vehicles That Cost At Least $100,000 Only
+        - Vehicles Released in 2014 Only
+        - Vehicles Listed for More Than 100 Days Only
+        - Manual Transmission Only
+    - The user can display a histogram for any of the columns in the dataset DataFrame (except for "is_4wd" or "date_posted").
+    - The user can display a scatterplot comparing vehicle prices with the type of vehicle, plotted over the different model year releases. 
+        The user is able to click on the different types of vehicles to filter what is displayed on the scatterplot.
--- app.py
+++ app.py
@@ -9,8 +9,43 @@ st.write('This is a small test application for running and ' +
         'displaying EDA results for a Pandas DataFrame onto ' + 
         'local server with Streamlit and Plotly. Here is the '  +
         'dataset that we will be working with:')
+factor1 = st.checkbox('Vehicles That Cost At Least $100,000 Only')
+factor2 = st.checkbox('Vehicles Released in 2014 Only')
+factor3 = st.checkbox('Vehicles Listed for More Than 100 Days Only')
+factor4 = st.checkbox('Manual Transmission Only')
+if factor1:
+    df_vehicles = df_vehicles[df_vehicles['price'] >= 100000]
+if factor2:
+    df_vehicles = df_vehicles[df_vehicles['model_year'] == 2014]
+if factor3:
+    df_vehicles = df_vehicles[df_vehicles['days_listed'] >= 100]
+if factor4:
+    df_vehicles = df_vehicles[df_vehicles['transmission'] == 'manual']
 st.write(df_vehicles)
-st.write('\n\n\n')
+count = len(df_vehicles.index)
+st.write('There are ' + str(count) + ' result(s) to show.')
 
-df_vehicles.info()
-df_vehicles.head()
+st.header('Vehicle Histograms', divider = 'rainbow')
+st.write('This first Plotly graph displays histograms for any applicable column ' + 
+        'for the vehicles in the dataset (not including \"is_4wd\" or \"date_posted\"):')
+applicable_columns = df_vehicles.columns.drop(['is_4wd', 'date_posted'])
+histogram_col = st.selectbox(
+    'Select a column to make a histogram from:',
+    applicable_columns)
+fig1 = px.histogram(df_vehicles, x=histogram_col)
+st.plotly_chart(fig1)
+
+st.header('Vehicle Scatterplots: Price vs. Type over Different Model Years', divider = 'rainbow')
+st.write('This second Plotly graph is a scatterplot comparing the price of different types of vehicles ' + 
+        'over different model year releases. You can select whichever type of vehicle to display in the scatterplot ' + 
+        'by clicking on a type in the legend:')
+fig2 = px.scatter(
+    df_vehicles,
+    x="model_year", 
+    y="price", 
+    labels=['Model Year', 'Price'], 
+    color="type",
+)
+st.plotly_chart(fig2)
+st.write('We can see from this scatterplot that there was a spike in price for sedans, coupes, and convertibles around 1965. ' + 
+        'Otherwise, the trends show a positive correlation between price and model year, regardless of the type of vehicle.')
