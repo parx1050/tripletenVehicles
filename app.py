aec6d7a2e28ed9a08076af23ea05cf47305fcbca
parx1050 <parker.shyu@gmail.com>
2024-03-05 03:59:35 -0500

    Removed the rainbow border for headers

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 df_vehicles = pd.read_csv('./vehicles_us.csv')
 
-st.header('Welcome to the Vehicles App!', divider = 'rainbow')
+st.header('Welcome to the Vehicles App!')
 st.write('This is a small test application for running and ' +
         'displaying EDA results for a Pandas DataFrame onto ' + 
         'local server with Streamlit and Plotly. Here is the '  +
@@ -25,7 +25,7 @@ st.write(df_vehicles)
 count = len(df_vehicles.index)
 st.write('There are ' + str(count) + ' result(s) to show.')
 
-st.header('Vehicle Histograms', divider = 'rainbow')
+st.header('Vehicle Histograms')
 st.write('This first Plotly graph displays histograms for any applicable column ' + 
         'for the vehicles in the dataset (not including \"is_4wd\" or \"date_posted\"):')
 applicable_columns = df_vehicles.columns.drop(['is_4wd', 'date_posted'])
@@ -35,7 +35,7 @@ histogram_col = st.selectbox(
 fig1 = px.histogram(df_vehicles, x=histogram_col)
 st.plotly_chart(fig1)
 
-st.header('Vehicle Scatterplots: Price vs. Type over Different Model Years', divider = 'rainbow')
+st.header('Vehicle Scatterplots: Price vs. Type over Different Model Years')
 st.write('This second Plotly graph is a scatterplot comparing the price of different types of vehicles ' + 
         'over different model year releases. You can select whichever type of vehicle to display in the scatterplot ' + 
         'by clicking on a type in the legend:')
